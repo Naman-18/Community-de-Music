872b59997d7c24555663d833708c9e71c4cf5162
Naman-18 <namanrockstar18@gmail.com>
2021-11-25 18:27:38 +0530

    Updated db

--- esocial/base/models.py
+++ esocial/base/models.py
@@ -7,7 +7,7 @@ from django.db.models.deletion import CASCADE
 class User(AbstractUser):
     name = models.CharField(max_length=200,null=True)
     email = models.EmailField(unique=True, null=True)
-    bio = models.TextField(null=True)
+    bio = models.TextField(null=True,blank = True)
     avatar = models.ImageField(null=True, default="avatar.svg")
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
